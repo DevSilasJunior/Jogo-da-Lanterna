dcb5912bab74f5e01b6665174c3bb31f65a13950
DevSilasJúnior <silasfigueredodasilvajunior@gmail.com>
2026-02-25 17:20:04 -0300

    novo update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
-let lanterna = document.getElementById("#overlay")
-lanterna.style.background = "radial-gradient( circle 100px yellow black )"
+let lanterna = document.getElementById("overlay")
 
+
+
+document.addEventListener("mousemove", function(e){
+
+    const Xcursor = e.clientX
+    const Ycursor = e.clientY
+    lanterna.style.background = `radial-gradient(circle 150px at ${Xcursor}px ${Ycursor}px, rgba(182, 182, 180, 0), rgba(225, 235, 183, 0.1), rgba(255, 255, 255, 0)`
+    console.log(lanterna)
+})
