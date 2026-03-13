ce9f53b5a1f180b378905d08adc9aa96c0f1ed81
DevSilasJúnior <silasfigueredodasilvajunior@gmail.com>
2026-03-13 09:13:44 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,10 @@ document.addEventListener("mousemove", function(e){
 
     const Xcursor = e.clientX
     const Ycursor = e.clientY
-    lanterna.style.background = `radial-gradient(circle 150px at ${Xcursor}px ${Ycursor}px, rgba(182, 182, 180, 0), rgba(225, 235, 183, 0.1), rgba(19, 17, 17, 0.97)`
+    lanterna.style.background = `radial-gradient(circle 150px at ${Xcursor}px ${Ycursor}px, rgba(182, 182, 180, 0), rgba(225, 235, 183, 0.1), rgba(19, 17, 17, 0)`
     console.log(lanterna)
+})
+
+document.addEventListener("click", function(e){
+    
 })
